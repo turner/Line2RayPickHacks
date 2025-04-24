86d7398bd7c804b0cca4426fbb3263d7102b485c
Douglass Turner <douglass.turner@gmail.com>
2025-04-24 16:49:18 -0400

    Raycast Service (#2)
    * Raycast Service
    
    * Refactor ray cast strategy
    
    * Added note about use of depth test and render order in conjunction with ray casting

--- src/sceneManager.js
+++ src/sceneManager.js
@@ -2,8 +2,10 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { generateSpiralPoints } from './utils.js';
 import LineFactory from './lineFactory.js'
+import RaycastService from './raycastService.js';
 
 const BACKGROUND_COLOR = 0xCACACA;
+const scratchColor = new THREE.Color();
 
 export class SceneManager {
 	constructor(container) {
@@ -18,12 +20,9 @@ export class SceneManager {
 
 		this.controls = new OrbitControls(this.camera, this.renderer.domElement);
 		this.setupControls();
-
+		
 		this.pointer = new THREE.Vector2();
-		this.raycaster = new THREE.Raycaster();
-		this.setupRaycaster();
-
-		this.color = new THREE.Color();
+		this.raycastService = new RaycastService();
 
 		this.setupScene();
 		this.setupEventListeners();
@@ -41,12 +40,7 @@ export class SceneManager {
 		this.controls.minDistance = 10;
 		this.controls.maxDistance = 500;
 	}
-
-	setupRaycaster() {
-		this.raycaster.params.Line2 = {};
-		this.raycaster.params.Line2.threshold = 0;
-	}
-
+			
 	setupScene() {
 		const spline = new THREE.CatmullRomCurve3(generateSpiralPoints(32))
 		this.spline = spline;
@@ -98,7 +92,6 @@ export class SceneManager {
 	}
 
 	setupEventListeners() {
-		document.addEventListener('pointermove', (event) => this.onPointerMove(event));
 		window.addEventListener('resize', () => this.onWindowResize());
 	}
 
@@ -107,8 +100,7 @@ export class SceneManager {
 		this.thresholdLine.position.copy(this.line.position);
 		this.thresholdLine.quaternion.copy(this.line.quaternion);
 
-		this.raycaster.setFromCamera(this.pointer, this.camera);
-		const lineIntersections = this.raycaster.intersectObject(this.line);
+		const lineIntersections = this.raycastService.intersectObject(this.camera, this.line);
 
 		if (lineIntersections.length > 0) {
 			this.handleIntersection(lineIntersections[0]);
@@ -124,22 +116,17 @@ export class SceneManager {
 		// this.sphereInter.visible = true;
 		// this.sphereInter.position.copy(intersection.point);
 
-		const index = intersection.faceIndex;
-		const colors = this.line.geometry.getAttribute('instanceColorStart');
-		this.color.fromBufferAttribute(colors, index);
+		const { faceIndex, pointOnLine, object:line } = intersection;
+
+		scratchColor.fromBufferAttribute(line.geometry.getAttribute('instanceColorStart'), faceIndex);
 
 		// Show feedback for line intersection
 		this.sphereOnLine.visible = true;
-		this.sphereOnLine.position.copy(intersection.pointOnLine);
-		this.sphereOnLine.material.color.copy(this.color).offsetHSL(0.7, 0, 0);
+		this.sphereOnLine.position.copy(pointOnLine);
+		this.sphereOnLine.material.color.copy(scratchColor).offsetHSL(0.7, 0, 0);
 
 		// Calculate parametric coordinate for the spiral
-		const t = this.findClosestT(
-			this.spline,
-			intersection.pointOnLine,
-			intersection.faceIndex,
-			this.line.geometry.getAttribute('instanceStart').count
-		);
+		const t = this.findClosestT(this.spline, pointOnLine, faceIndex, line.geometry.getAttribute('instanceStart').count);
 		console.log('Segment index (t):', t);
 
 		this.renderer.domElement.style.cursor = 'crosshair';
@@ -155,11 +142,6 @@ export class SceneManager {
 		this.camera.aspect = window.innerWidth / window.innerHeight;
 		this.camera.updateProjectionMatrix();
 		this.renderer.setSize(window.innerWidth, window.innerHeight);
-	}
-
-	onPointerMove(event) {
-		this.pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
-		this.pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
 	}
 
 	findClosestT(spline, targetPoint, segmentIndex, totalSegments, tolerance = 0.0001) {
